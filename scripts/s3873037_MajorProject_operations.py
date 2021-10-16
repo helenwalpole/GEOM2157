818b924b75a89cd2262c1bf9d31221863b27d1b5
Helen Walpole <66713365+helenwalpole@users.noreply.github.com>
2021-10-16 16:43:59 +1100

    Criteria 1-4 complete. Nulls not yet resolved.

--- scripts/s3873037_MajorProject_operations.py
+++ scripts/s3873037_MajorProject_operations.py
@@ -7,6 +7,7 @@
 import processing
 #import math
 from qgis.PyQt import QtGui
+from PyQt5.QtCore import QVariant
 
 
 # Create variables.
@@ -14,15 +15,33 @@ from qgis.PyQt import QtGui
 # selected by the user in the tool UI. 
 
 # Set user-adjustable variables
-stationThreshold = 1000
+stationsThreshold = 1000
 toiletsThreshold = 200
-hillsWeight = 6
-parklandThreshold = 15
+drinkTapsThreshold = 200
+playgroundsThreshold = 50
+boundariesThreshold = 200
+recentUseThreshold = 1
+linearFeaturesThreshold = 200
+riversThreshold = 1
+parklandsThreshold = 15
+hillsThreshold = 8 #fixed
+
+# on a scale of 1-5, how important is: 
+stationsWeight = 1
+toiletsWeight = 5
+drinkTapsWeight = 1
+playgroundsWeight = 1
+boundariesWeight = 3
+recentUseWeight = 5
+linearFeaturesWeight = 1
+riversWeight = 2
+parklandsWeight = 5
+hillsWeight = 5
+startsWeight = 3
 
 # Set the fixed variables
-freewayThreshold = 200
+freewaysThreshold = 200
 riverThreshold = True
-
 
 # Set filepaths
 inputFP = 'C:/Users/helen/Documents/Assignment5/inputData/'
@@ -30,40 +49,35 @@ sourceFP = 'C:/Users/helen/Documents/Assignment5/sourceData/'
 processingFP = 'C:/Users/helen/Documents/Assignment5/OPTDATA2/'
 
 # Create two dictionaries from the user input to guide suitability criteria thresholds and weights.
-thresholdsDict['stations'] = stationThreshold
-weightsDict['stations'] = 
+thresholdsDict = {
+    'stations' : stationsThreshold, 
+    'toilets' : toiletsThreshold, 
+    'drinkTaps' : drinkTapsThreshold, 
+    'playgrounds' : playgroundsThreshold, 
+    'recentUse' : recentUseThreshold, 
+    'linearFeatures' : linearFeaturesThreshold, 
+    'rivers' : riversThreshold, 
+    'parklands' : parklandsThreshold, 
+    'hills' : hillsThreshold}
 
-thresholdsDict['toilets'] = toiletsThreshold
-weightsDict['toilets'] = 
-
-thresholdsDict['drinkTaps'] = 
-weightsDict['drinkTaps'] = 
-
-thresholdsDict['playgrounds'] = 
-weightsDict['playgrounds'] = 
-
-thresholdsDict['recentUse'] = 
-weightsDict['recentUse'] = 
-    
-thresholdsDict['hills'] = hillsThreshold
-weightsDict['hills'] = 
-
-thresholdsDict['parkland'] = 
-weightsDict['parkland'] = 
-
+weightsDict = {
+    'stations' : stationsWeight, 
+    'toilets' : toiletsWeight, 
+    'drinkTaps' : drinkTapsWeight, 
+    'playgrounds' : playgroundsWeight, 
+    'recentUse' : recentUseWeight, 
+    'linearFeatures' : linearFeaturesWeight, 
+    'rivers' : riversWeight, 
+    'parklands' : parklandsWeight, 
+    'hills' : hillsWeight,
+    'starts' : startsWeight}
 
 # Create pointers to the input layers 
 compAreasLayer = iface.addVectorLayer((inputFP + 'compAreas_OV.shp'), 'Layer', 'ogr')
 startLocationsLayer = iface.addVectorLayer((inputFP + 'startLocations_OV.shp'), 'Layer', 'ogr')
 
 # PREPARE DATA LAYERS
-# Derive a new layer of competiton area boundaries from the compAreas polygon layer for later use. 
-paramDict = {
-    'INPUT' : (inputFP + 'compAreas_OV.shp'), 
-    'OUTPUT': (sourceFP + 'boundaries.shp')
-    }
-processing.run('native:polygonstolines', paramDict)
-#compOutlinesLayer = iface.addVectorLayer((sourceFP + 'boundaries.shp'), 'Layer', 'ogr')
+
 
 
 # SUITABILITY ANALYSIS: START LOCATIONS
@@ -85,17 +99,17 @@ for layerIndex, layerString in enumerate(layersList):
     
     # Create a dictionary of parameters for the algorithm specific to each item in the loop.
     paramDict = { 
-    'DISCARD_NONMATCHING' : False, 
-    'FIELDS_TO_COPY' : ['FID'], #all layers have this field 
     'INPUT' : inputParam, #except for first iteration, this takes the output layer from the previous loop as input
+    'DISCARD_NONMATCHING' : False, 
     'INPUT_2' : (sourceFP + layerString + '.shp'), #takes the correct join layer for this iteration
+    'FIELDS_TO_COPY' : ['FID'], #all layers have this field 
     'MAX_DISTANCE' : None, 
     'NEIGHBORS' : 1, 
     'OUTPUT' : (processingFP + layerString + 'Join.shp'), 
     'PREFIX' : layerString[0:5].upper() + '_'
     }
     print(paramDict)
-    # Run the joinbynearest algorithm using the new dictionary
+    # Run the joinbynearest algorithm using the dictionary
     processing.run('native:joinbynearest', paramDict)
     
     # The joinbynearest algorithm adds new fields by default that we do not need. 
@@ -125,61 +139,235 @@ print('Done with Start Locations loop')
 
 #########################
 # if doing distance to boundaries, do it here#
-############################
+
+# Derive a new layer of competiton area boundaries from the compAreas polygon layer for later use. 
+paramDict = {
+    'INPUT' : (inputFP + 'compAreas_OV.shp'), 
+    'OUTPUT': (sourceFP + 'boundaries.shp')
+    }
+processing.run('native:polygonstolines', paramDict)
+#compOutlinesLayer = iface.addVectorLayer((sourceFP + 'boundaries.shp'), 'Layer', 'ogr')
+
+# do a spatial join
+# remove duplicates
 
 # Create a pointer to the final layer
-startLocationsProcessed = iface.addVectorLayer((processingFP + 'startLocationsCrit' + str(len(layersList)) + '.shp'), 'startLocations', 'ogr')
+startLocationsProcessed = iface.addVectorLayer((processingFP + 'startLocationsCrit' + str(len(layersList)) + '.shp'), 'startLocationsProcessed', 'ogr')
 
 # Use input data to calcuate thresholds, allocate indices, and calculate weights.
 # Could potentially do this inside the processing loop, checking thresholds and weights, 
 # but require dictionaries to be already built.
 
+############################
+
 # Process suitability of competition areas: 
-# Criteria 1: join table with usage 
+# Criteria 1: join compAreas layer to the table of recently used compAreas in the previous season
 paramDict = {
+    'INPUT' : (inputFP + 'compAreas_OV.shp'), #replace with param 
     'DISCARD_NONMATCHING' : False, 
-    'INPUT' : 'C:/Users/helen/Documents/Assignment5/inputData/compAreas_OV.shp', 
     'FIELD' : 'Map_Name', 
-    'INPUT_2' : 'C:/Users/helen/Documents/Assignment5/inputData/compAreaUse2019_OV.csv', 
+    'INPUT_2' : (inputFP + 'compAreaUse2019_OV.csv'), #replace with param 
     'FIELD_2' : 'MapName', 
     'FIELDS_TO_COPY' : ['UsedInSeason'], 
     'METHOD' : 1, 
-    'OUTPUT' : 'C:/Users/helen/Documents/Assignment5/OPTDATA1/compAreas1.shp', 
+    'OUTPUT' : (processingFP + 'compAreas1.shp'), 
     'PREFIX' : '' 
     }
 processing.run('native:joinattributestable', paramDict)
 #Create a pointer to the new layer for Criteria 1
-compAreasCriteria1 = QgsVectorLayer()
+compAreasCriteria1 = QgsVectorLayer((processingFP + 'compAreas1.shp'), '', 'ogr')
 
 # Criteria 2: check for freeways, roads or trainlines more than 200m from boundary
-# Create a new buffered vector layer of the compeition areas where boundaries are reduced by 200m
+# Create a new buffered vector layer of the competition areas where boundaries are reduced by 200m
 paramDict = {
-    'INPUT' : 'C:/Users/helen/Documents/Assignment5/inputData/compAreas_OV.shp', 
-    'DISTANCE' : -200, 
-    'SEGMENTS' : 5 
+    'INPUT' : compAreasCriteria1, #replace with param 
+    'DISTANCE' : -linearFeaturesThreshold, 
+    'SEGMENTS' : 5, 
     'END_CAP_STYLE' : 0, 
     'JOIN_STYLE' : 0, 
     'MITER_LIMIT' : 2, 
     'DISSOLVE' : False, 
-    'OUTPUT' : 'C:/Users/helen/Documents/Assignment5/outputData/compAreas200m.shp', 
+    'OUTPUT' : (processingFP + 'compAreas200m.shp'), 
     }
 processing.run('native:buffer', paramDict)
 # Create a pointer to the buffered layer
-compAreasBuffered = QgsVectorLayer()
+compAreasBuffered = QgsVectorLayer((processingFP + 'compAreas200m.shp'), '', 'ogr')
 
-# Criteria 3: check for rivers
-# Criteria 4: calculate area of parkland
-# Criteria 5: intersect contours, sum their length, divide by area and 
+############# LOOP THROUGH RAIL, FREEWAYS AND ROADS?##########
 
+#Check to see if train lines share any geometry with any features in this buffered layer. 
+paramDict = { 
+    'INPUT' : compAreasBuffered, 
+    'DISCARD_NONMATCHING' : False, 
+    'INPUT_2' : (sourceFP + 'trainlines.shp'), 
+    'FIELDS_TO_COPY' : ['FID'], 
+    'MAX_DISTANCE' : None, 
+    'NEIGHBORS' : 1, 
+    'OUTPUT' : (processingFP + 'trainlinesJoinDuplicates.shp'), 
+    'PREFIX' : 'RAIL_' 
+    }
+# Run the joinbynearest algorithm using the dictionary
+processing.run('native:joinbynearest', paramDict)
+
+# The joinbynearest algorithm creates duplicate features if there is more than one 'nearest' feature inside the polygon. 
+# Delete duplicate records. 
+paramDict = { 
+    'FIELDS' : ['Map_Name'], 
+    'INPUT' : (processingFP + 'trainlinesJoinDuplicates.shp'), 
+    'OUTPUT' : (processingFP + 'trainlinesJoin.shp') 
+    }
+processing.run('native:removeduplicatesbyattribute', paramDict)
+    
+# Remove extra fields added by the joinbynearest algorithm. 
+#paramDict = { 
+#    'COLUMN' : ['feature_x','feature_y','nearest_x','nearest_y', 'n', (layerString[0:5].upper() + '_' + 'FID')], 
+#    'INPUT' : (processingFP + 'trainlinesJoin.shp'), 
+#    'OUTPUT' : (processingFP + 'compAreas2.shp') 
+#    }
+#processing.run('native:deletecolumn', paramDict)
+
+# Rename the newly-created distance field so we know it relates to trainlines.
+joinedLayer = QgsVectorLayer((processingFP + 'trainlinesJoin.shp'), '', 'ogr')
+# Loop through the fields in this layer to find 'distance'
+for field in joinedLayer.fields():
+    if field.name() == 'distance':
+        with edit(joinedLayer):
+            # Find the index number of the distance field.
+            idx = joinedLayer.fields().indexFromName(field.name())
+            # Use the index to identify and rename the field with a prefix relating to this iteration. 
+            joinedLayer.renameAttribute(idx, 'RAIL_DIST')
+
+# Join the RAIL_DIST field to the compAreas layer, as the above operations were performed on the buffered layer. 
+# This is a 1:1 join, so we can use the joinattributestable algorithm. 
+paramDict = {
+    'INPUT' : compAreasCriteria1, #replace with param 
+    'DISCARD_NONMATCHING' : False, 
+    'FIELD' : 'FID', 
+    'INPUT_2' : joinedLayer, 
+    'FIELD_2' : 'FID', 
+    'FIELDS_TO_COPY' : ['RAIL_DIST'], 
+    'METHOD' : 1, 
+    'OUTPUT' : (processingFP + 'compAreas2.shp'), 
+    'PREFIX' : '' 
+    }
+processing.run('native:joinattributestable', paramDict)
+
+# Create a pointer for this layer so we can use it again. 
+compAreasCriteria2 = QgsVectorLayer((processingFP + 'compAreas2.shp'), '', 'ogr')
+
+# Criteria 3: Check for rivers
+# compAreasCriteria3 = QgsVectorLayer(XXXXXXXXXXXXX)
+compAreasCriteria3 = compAreasCriteria2
+
+# Criteria 4: Calculate proportion of competition area covered by parkland.
+# Intersect the parkland with the compAreas so that we have accurate measurements of park area within each polygon. 
+paramDict = { 
+    'INPUT' : (sourceFP + 'parkland.shp'), 
+    'INPUT_FIELDS' : [], 
+    'OVERLAY' : compAreasCriteria3, 
+    'OVERLAY_FIELDS' : ['FID'], 
+    'OVERLAY_FIELDS_PREFIX' : '', 
+    'OUTPUT' : (processingFP + 'parklandIntersect.shp'), 
+    }
+processing.run('native:intersection', paramDict)
+# Create a pointer for this layer so we can use it again. 
+
+# Use a spatial join to sum the area of parkland in each competition area
+paramDict = {
+    'INPUT' : compAreasCriteria3, 
+    'JOIN' : (processingFP + 'parklandIntersect1.shp'), 
+    'PREDICATE' : [0], 
+    'JOIN_FIELDS' : ['Shape_Area'], 
+    'SUMMARIES' : [5], 
+    'DISCARD_NONMATCHING' : False, 
+    'PREFIX': 'PARK_', 
+    'OUTPUT' : (processingFP + 'parklandSums.shp'), 
+    }
+processing.run('qgis:joinbylocationsummary', paramDict)
+# Create a pointer for this layer so we can use it again. 
+joinedLayer = QgsVectorLayer((processingFP + 'parklandSums.shp'), '', 'ogr')
+
+# Update each feature with the ratio of parkland to the total area of the compArea polygon. 
+for i in joinedLayer.getFeatures():
+    # Capture attribute values so we can replace NULLs with zeroes. 
+    if i['UsedInSeas'] != NULL: 
+        usedInSeason = int(i['UsedInSeas'])
+    else: 
+        usedInSeason = int(0)
+    #print('usedInSeason is: ', usedInSeason)
+        
+    if i['Shape_Ar_1'] != NULL: 
+        fltParkArea = float(i['Shape_Ar_1'])
+    else: 
+        fltParkArea = float(0)
+    
+    # Calculate the ratio of parkland 
+    parkRatio = fltParkArea / i['Shape_Area']
+    #print("Park ratio is: ", parkRatio)
+    
+    # overwrite the 'Shape_Ar_1' field with the new ratio value, and replace NULLs with 0s
+    joinedLayer.startEditing()
+    i['Shape_Ar_1'] = parkRatio
+    i['UsedInSeas'] = usedInSeason
+    joinedLayer.updateFeature(i)
+    joinedLayer.commitChanges()
+    ##NOTE: THIS PART OF THE SCRIPT IS VERY SLOW AND CAUSES A LONG LAG##
+
+# Loop through the fields and update the name of 'Shape_Ar_1' to 'parkRatio'.
+for field in joinedLayer.fields():
+    if field.name() == 'Shape_Ar_1': 
+        with edit(joinedLayer):
+            # Get the index number of the 'Shape_Ar_1' field 
+            idx = joinedLayer.fields().indexFromName(field.name())
+            # Use the index to identify and rename the field
+            joinedLayer.renameAttribute(idx, 'PARK_RATIO')
+
+# Create a pointer for this layer so we can use it again. 
+compAreasCriteria4 = joinedLayer 
+
+########no syntax or runtime errors to here. Check for logic errors. #############
+##### NO ERRORS TO HERE. NEED TO FIX NULLS IN SHAPE_AR_1 ###########
+
+# Criteria 5: Intersect contours, sum their length, divide by area and 
+# Intersect the parkland with the compAreas so that we have accurate measurements of park area within each polygon. 
+paramDict = { 
+    'INPUT' : (sourceFP + 'contours.shp'), 
+    'INPUT_FIELDS' : [], 
+    'OVERLAY' : compAreasCriteria4, 
+    'OVERLAY_FIELDS' : ['FID'], 
+    'OVERLAY_FIELDS_PREFIX' : '', 
+    'OUTPUT' : (processingFP + 'contoursTrimmed.shp')
+    }
+processing.run('native:intersection', paramDict)
+
+# Use a spatial join to sum the area of parkland in each competition area
+paramDict = {
+    'INPUT' : 'C:/Users/helen/Documents/Assignment5/inputData/compAreas_OV.shp', 
+    'JOIN' : 'C:/Users/helen/Documents/Assignment5/OPTDATA1/parklandIntersect.shp', 
+    'PREDICATE' : [0], 
+    'JOIN_FIELDS' : ['Shape_Area'], 
+    'SUMMARIES' : [5], 
+    'DISCARD_NONMATCHING' : False, 
+    'PREFIX': 'PARK_', 
+    'OUTPUT' : 'C:/Users/helen/Documents/Assignment5/OPTDATA2/parklandSums.shp', 
+    }
+processing.run('qgis:joinbylocationsummary', paramDict)
+
+# Create a pointer for this layer so we can use it again. 
+compAreasAllCriteria = QgsVectorLayer(XXXXXXXXXXXXX)
+
+
+
+### SUITABILITY ANALYSIS CALCULATIONS
+# Use mathematical operations to calculate final suitability scores 
 
 
 
 for alg in QgsApplication.processingRegistry().algorithms():
-    if "join" in alg.id():
+    if "location" in alg.id():
         print(alg.id(), "-->", alg.displayName())
 
-
-processing.algorithmHelp("native:buffer")
+processing.algorithmHelp('native:intersection')
 
 
 
