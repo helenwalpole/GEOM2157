4a6de5818dc8bcfee8750167f538c85b37c21b1f
Helen Walpole <66713365+helenwalpole@users.noreply.github.com>
2021-10-16 11:34:55 +1100

    Completed criteria 4 and 5 on compAreas

--- scripts/s3873037_MajorProject_operations.py
+++ scripts/s3873037_MajorProject_operations.py
@@ -16,44 +16,72 @@ from qgis.PyQt import QtGui
 # Set user-adjustable variables
 stationThreshold = 1000
 toiletsThreshold = 200
-hillsRating = 6
+hillsWeight = 6
 parklandThreshold = 15
+
+# Set the fixed variables
+freewayThreshold = 200
+riverThreshold = True
+
 
 # Set filepaths
 inputFP = 'C:/Users/helen/Documents/Assignment5/inputData/'
 sourceFP = 'C:/Users/helen/Documents/Assignment5/sourceData/'
-processingFP = 'C:/Users/helen/Documents/Assignment5/outputData/'
+processingFP = 'C:/Users/helen/Documents/Assignment5/OPTDATA2/'
 
-# Add OV input layers to the map
+# Create two dictionaries from the user input to guide suitability criteria thresholds and weights.
+thresholdsDict['stations'] = stationThreshold
+weightsDict['stations'] = 
+
+thresholdsDict['toilets'] = toiletsThreshold
+weightsDict['toilets'] = 
+
+thresholdsDict['drinkTaps'] = 
+weightsDict['drinkTaps'] = 
+
+thresholdsDict['playgrounds'] = 
+weightsDict['playgrounds'] = 
+
+thresholdsDict['recentUse'] = 
+weightsDict['recentUse'] = 
+    
+thresholdsDict['hills'] = hillsThreshold
+weightsDict['hills'] = 
+
+thresholdsDict['parkland'] = 
+weightsDict['parkland'] = 
+
+
+# Create pointers to the input layers 
 compAreasLayer = iface.addVectorLayer((inputFP + 'compAreas_OV.shp'), 'Layer', 'ogr')
 startLocationsLayer = iface.addVectorLayer((inputFP + 'startLocations_OV.shp'), 'Layer', 'ogr')
 
-# Conduct suitability analysis on start locations. 
-# Establish a list of operations to guide our loop. 
-#operationsList = ['nearTrainStations', 'nearEdgeOfArea', 'nearToilets', 'nearTaps', 'nearPlaygrounds']
-
-# Derive a new layer of competiton area boundaries from the 
-# compAreas polygon layer for use in the 'nearEdgeOfArea' operation. 
+# PREPARE DATA LAYERS
+# Derive a new layer of competiton area boundaries from the compAreas polygon layer for later use. 
 paramDict = {
     'INPUT' : (inputFP + 'compAreas_OV.shp'), 
     'OUTPUT': (sourceFP + 'boundaries.shp')
     }
 processing.run('native:polygonstolines', paramDict)
-compOutlinesLayer = iface.addVectorLayer((sourceFP + 'boundaries.shp'), 'Layer', 'ogr')
+#compOutlinesLayer = iface.addVectorLayer((sourceFP + 'boundaries.shp'), 'Layer', 'ogr')
 
+
+# SUITABILITY ANALYSIS: START LOCATIONS
 # We will calcuate distance of each start location from various features using 'joinbynearest'. 
-# We will loop this operation five times for the different types of features. 
-layersList = ['stations', 'boundaries', 'toilets', 'drinkTaps', 'playgrounds']
+# We will loop this operation for each of the different types of features we are measuring. 
+
+###########
+layersList = ['stations', 'toilets', 'drinkTaps', 'playgrounds']
+processingFP = 'C:/Users/helen/Documents/Assignment5/OPTDATA2/'
 
 for layerIndex, layerString in enumerate(layersList):
     # Set the input filepath for the first iteration to take the file from the 'inputFP' folder. 
     # After that, take files from the processing folder
-    #print(layerIndex, layerString)
+    print(layerIndex, layerString)
     if layerIndex == 0: 
         inputParam = (inputFP + 'startLocations_OV.shp')
     else: 
-        inputParam = (processingFP + 'startLocations' + str(layerIndex) + '.shp')
-    #print(type(inputParam))
+        inputParam = (processingFP + 'startLocationsCrit' + str(layerIndex) + '.shp')
     
     # Create a dictionary of parameters for the algorithm specific to each item in the loop.
     paramDict = { 
@@ -71,31 +99,87 @@ for layerIndex, layerString in enumerate(layersList):
     processing.run('native:joinbynearest', paramDict)
     
     # The joinbynearest algorithm adds new fields by default that we do not need. 
-    # To avoid confusion, we will remove these extra fields as we go. 
+    # To avoid issues arising from successive iterations overwriting these fields,
+    # we will remove these extra fields as we go. 
     paramDict = { 
-        'COLUMN' : ['feature_x','feature_y','nearest_x','nearest_y'], 
+        'COLUMN' : ['feature_x','feature_y','nearest_x','nearest_y', 'n', (layerString[0:5].upper() + '_' + 'FID')], 
         'INPUT' : (processingFP + layerString + 'Join.shp'), 
-        'OUTPUT' : (processingFP + 'startLocations' + str(layerIndex + 1) + '.shp') 
+        'OUTPUT' : (processingFP + 'startLocationsCrit' + str(layerIndex + 1) + '.shp') 
         }
     processing.run('native:deletecolumn', paramDict)
-       
+    
+    # The joinbynearest algorithm has created a new field called 'distance'. 
+    # We need to rename this field to keep track of which feature the distance relates to. 
+    # First, create a pointer to the new layer we have created
+    joinedLayer = QgsVectorLayer((processingFP + 'startLocationsCrit' + str(layerIndex + 1) + '.shp'), '', 'ogr')
+    # Loop through the fields in this layer to find 'distance'
+    for field in joinedLayer.fields():
+        if field.name() == 'distance':
+            with edit(joinedLayer):
+                # Find the index number of the distance field.
+                idx = joinedLayer.fields().indexFromName(field.name())
+                # Use the index to identify and rename the field with a prefix relating to this iteration. 
+                joinedLayer.renameAttribute(idx, (layerString[0:5].upper() + '_DIST'))
+
+print('Done with Start Locations loop')
+
+#########################
+# if doing distance to boundaries, do it here#
+############################
+
 # Create a pointer to the final layer
-startLocationsProcessed = iface.addVectorLayer((processingFP + 'startLocations' + str(len(layersList)) + '.shp'), 'startLocations', 'ogr')
+startLocationsProcessed = iface.addVectorLayer((processingFP + 'startLocationsCrit' + str(len(layersList)) + '.shp'), 'startLocations', 'ogr')
 
-# Create two dictionaries that match the suitability criteria for start locations 
-# to their threshold values and to their weights, using the data input by the user.
+# Use input data to calcuate thresholds, allocate indices, and calculate weights.
+# Could potentially do this inside the processing loop, checking thresholds and weights, 
+# but require dictionaries to be already built.
 
-thresholdDict = {}
-weightDict = {}
+# Process suitability of competition areas: 
+# Criteria 1: join table with usage 
+paramDict = {
+    'DISCARD_NONMATCHING' : False, 
+    'INPUT' : 'C:/Users/helen/Documents/Assignment5/inputData/compAreas_OV.shp', 
+    'FIELD' : 'Map_Name', 
+    'INPUT_2' : 'C:/Users/helen/Documents/Assignment5/inputData/compAreaUse2019_OV.csv', 
+    'FIELD_2' : 'MapName', 
+    'FIELDS_TO_COPY' : ['UsedInSeason'], 
+    'METHOD' : 1, 
+    'OUTPUT' : 'C:/Users/helen/Documents/Assignment5/OPTDATA1/compAreas1.shp', 
+    'PREFIX' : '' 
+    }
+processing.run('native:joinattributestable', paramDict)
+#Create a pointer to the new layer for Criteria 1
+compAreasCriteria1 = QgsVectorLayer()
 
-for 
+# Criteria 2: check for freeways, roads or trainlines more than 200m from boundary
+# Create a new buffered vector layer of the compeition areas where boundaries are reduced by 200m
+paramDict = {
+    'INPUT' : 'C:/Users/helen/Documents/Assignment5/inputData/compAreas_OV.shp', 
+    'DISTANCE' : -200, 
+    'SEGMENTS' : 5 
+    'END_CAP_STYLE' : 0, 
+    'JOIN_STYLE' : 0, 
+    'MITER_LIMIT' : 2, 
+    'DISSOLVE' : False, 
+    'OUTPUT' : 'C:/Users/helen/Documents/Assignment5/outputData/compAreas200m.shp', 
+    }
+processing.run('native:buffer', paramDict)
+# Create a pointer to the buffered layer
+compAreasBuffered = QgsVectorLayer()
+
+# Criteria 3: check for rivers
+# Criteria 4: calculate area of parkland
+# Criteria 5: intersect contours, sum their length, divide by area and 
 
 
-#for alg in QgsApplication.processingRegistry().algorithms():
-#    if "field" in alg.displayName():
-#        print(alg.id(), "-->", alg.displayName())
-#        
-#processing.algorithmHelp("native:deletecolumn")
+
+
+for alg in QgsApplication.processingRegistry().algorithms():
+    if "join" in alg.id():
+        print(alg.id(), "-->", alg.displayName())
+
+
+processing.algorithmHelp("native:buffer")
 
 
 
